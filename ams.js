6fbd7d57320dac881ec648e69d9d26841d50c86f
David Schlachter <david+gh@schlachter.ca>
2019-02-15 02:35:11 -0500

    Add basic storage persistence

--- ams.js
+++ ams.js
@@ -1,24 +1,27 @@
 const mm = require('music-metadata')
+const Store = require('electron-store')
+const store = new Store()
+
+// Get the last folder used on launch
+let folder
+if (store.has("folder")) {
+    let folder = store.get("folder")
+    document.getElementById("last").innerHTML = "Last loaded " + folder
+    let metadata = getMetadata(folder)
+    console.log(metadata)
+    document.getElementById("last").innerHTML = "Loaded " + folder
+}
 
 document.addEventListener('drop', function (e) {
-e.preventDefault()
-e.stopPropagation()
-for (let f of e.dataTransfer.files) {
-		let filelist = new Array()
-		walkSync(f.path, filelist)
-		console.log(filelist)
-		for (let i = 0; i < filelist.length; i++) {
-			if (filelist[i].includes(".m4a") || filelist[i].includes(".mp4")) {
-				mm.parseFile(filelist[i], {native: true})
-				  .then( metadata => {
-				    console.log(metadata);
-				  })
-				  .catch( err => {
-				    console.error(err.message);
-				  });
-			}
-		}
-}
+    e.preventDefault()
+    e.stopPropagation()
+    for (let f of e.dataTransfer.files) {
+        let folder = f.path
+        store.set("folder", folder)
+	    let metadata = getMetadata(folder)
+        document.getElementById("last").innerHTML = "Loaded " + folder
+        console.log(metadata)
+    }
 })
 document.addEventListener('dragover', function (e) {
 e.preventDefault()
@@ -41,4 +44,23 @@ function walkSync (dir, filelist) {
 	}
 	})
 	return filelist
+}
+
+function getMetadata(dir) {
+	let filelist = new Array()
+    let data = new Array()
+	walkSync(dir, filelist)
+	console.log(filelist)
+	for (let i = 0; i < filelist.length; i++) {
+		if (filelist[i].includes(".m4a") || filelist[i].includes(".mp4")) {
+			mm.parseFile(filelist[i], {native: true})
+			  .then( metadata => {
+			    data.push(metadata)
+			  })
+			  .catch( err => {
+			    console.error(err.message)
+			  });
+		}
+	}
+    return data
 }
