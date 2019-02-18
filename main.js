c91dbedcd1731e73a253bc9914be87d42a507255
David Schlachter <david+gh@schlachter.ca>
2019-02-18 10:22:50 -0500

    Remember window position

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 const { app, BrowserWindow, globalShortcut } = require('electron')
 
+const Store = require('electron-store')
+const store = new Store()
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
 function createWindow () {
+	let width = 800
+	let height = 600
+	let x = 0
+	let y = 0
+	if (store.has("dimensions")) {
+		let dimensions = store.get("dimensions")
+		width = dimensions.width
+		height = dimensions.height
+		x = dimensions.x
+		y = dimensions.y
+	}
 	// Create the browser window.
-	win = new BrowserWindow({webPreferences: {nodeIntegration: true}, width: 1300, height: 750})
+	win = new BrowserWindow({webPreferences: {nodeIntegration: true}, width: width, height: height, x: x, y: y})
 
 	// and load the index.html of the app.
 	win.loadFile('index.html')
@@ -20,6 +34,10 @@ function createWindow () {
 	})
 	globalShortcut.register('medianexttrack', () => {
 		win.webContents.send('nextlistener', 'nexttrack')
+	})
+
+	win.on('resize', () => {
+		store.set("dimensions", win.getBounds())
 	})
 
 	// Emitted when the window is closed.
